8d9eabef918b449e00859980c61162b5304b3385
Patrik Horvath <ferencpatrikhorvath@gmail.com>
2025-02-13 21:11:32 +0100

    case 4 impl

---
+++ src/main/java/com/genesys/framework/selenium/pom/guru99/Guru99TooltipPage.java
@@ -0,0 +1,25 @@
+package com.genesys.framework.selenium.pom.guru99;
+
+import com.genesys.framework.selenium.WebDriverFactory;
+import com.genesys.framework.selenium.pom.BasePage;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Guru99TooltipPage extends BasePage {
+
+    @FindBy(how = How.ID, using = "download_now")
+    private WebElement downloadNowButton;
+
+    protected Guru99TooltipPage(final WebDriverFactory driverFactory) {
+        super(driverFactory);
+    }
+
+    @Override
+    public boolean isAt() {
+        return downloadNowButton.isDisplayed();
+    }
+
+}
