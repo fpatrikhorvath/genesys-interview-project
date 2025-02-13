60b809e402ba7e236d765edb876f0c708a666832
Patrik Horvath <ferencpatrikhorvath@gmail.com>
2025-02-13 16:25:49 +0100

    removed unneces. pom dependencies

--- src/main/java/com/genesys/framework/runner/TestRunner.java
+++ src/main/java/com/genesys/framework/runner/TestRunner.java
@@ -4,12 +4,8 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 import org.testng.annotations.DataProvider;
 
-@CucumberOptions(
-        features = {"classpath:features"},
-        glue = {"com.genesys.framework.stepdefs"},
-        tags = "@ui"
-)
-public class UiTestRunner extends AbstractTestNGCucumberTests {
+@CucumberOptions(features = {"classpath:features"}, glue = {"com.genesys.framework.stepdefs"})
+public class TestRunner extends AbstractTestNGCucumberTests {
     @Override
     @DataProvider(parallel = false)
     public Object[][] scenarios() {
